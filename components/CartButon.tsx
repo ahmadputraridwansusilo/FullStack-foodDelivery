ca49f0d165ca17b7dfba7a1053fba196e4a47ad0
ahmadputraridwansusilo <tzyyyiyupa@gmail.com>
2025-07-05 18:33:56 +0700

    first commit

---
+++ components/CartButon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CartButon = () => {
+  return (
+    <View>
+      <Text>CartButon</Text>
+    </View>
+  )
+}
+
+export default CartButon
